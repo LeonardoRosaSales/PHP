5b89d20da05325cdd83355f4ec7123451bca6921
Leonardo <leonardo.rosa_2203@outlook.com>
2024-08-20 15:02:56 -0300

    Registro - OK

--- 01 - Help Desk/admin/valida_login.php
+++ 01 - Help Desk/admin/valida_login.php
@@ -18,15 +18,18 @@
     session_start();
 
     $usuario_autenticado = false;
+    $usuario_id = null;
 
     $usuarios_app = array(
-        array('email' => 'adm@teste.com.br', 'senha' => '1234'),
-        array('email' => 'user@teste.com.br', 'senha' => 'abcd')
+        array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '1234'),
+        array('id' => 2, 'email' => 'user@teste.com.br', 'senha' => '1234'),
+        array('id' => 3, 'email' => 'leo@leo.com.br', 'senha' => '1234')
     );
 
     foreach ($usuarios_app as $user) {
       if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
         $usuario_autenticado = true;
+        $usuario_id = $user['id'];
       };
     };
 
@@ -34,6 +37,7 @@
         // echo 'Usuário autenticado com sucesso!';
 
         $_SESSION['autenticado'] = 'SIM';
+        $_SESSION['id'] = $usuario_id;
         header('Location: home.php');
     }else{
         // echo 'Erro de Auteticação';
