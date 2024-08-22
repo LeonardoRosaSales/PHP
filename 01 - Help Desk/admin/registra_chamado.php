3c473d80d55859bf6703353e91dabd391413055c
Leonardo <leonardo.rosa_2203@outlook.com>
2024-08-22 16:59:11 -0300

    Help Desk - OK

--- 01 - Help Desk/admin/registra_chamado.php
+++ 01 - Help Desk/admin/registra_chamado.php
@@ -8,9 +8,9 @@ session_start();
 
 // $texto = $_POST['titulo'] . $_POST['categoria'] . $_POST['descricao'];
 
-$titulo = str_replace('#','-', $_POST['titulo']);
-$categoria = str_replace('#','-', $_POST['categoria']);
-$descricao = str_replace('#','-', $_POST['descricao']);
+$titulo = str_replace('#', '-', $_POST['titulo']);
+$categoria = str_replace('#', '-', $_POST['categoria']);
+$descricao = str_replace('#', '-', $_POST['descricao']);
 
 $texto = $_SESSION['id'] . '#' . $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
 
--- 01 - Help Desk/admin/valida_login.php
+++ 01 - Help Desk/admin/valida_login.php
@@ -1,56 +1,59 @@
 <?php
-    // echo 'Teste Ok!';
+// echo 'Teste Ok!';
 
-    // print_r($_GET);
+// print_r($_GET);
 
-    // echo '<br>';
-    // echo $_GET['email'];
-    // echo '<br>';
-    // echo $_GET['senha'];
+// echo '<br>';
+// echo $_GET['email'];
+// echo '<br>';
+// echo $_GET['senha'];
 
-    // print_r($_POST);
+// print_r($_POST);
 
-    // echo '<br>';
-    // echo $_POST['email'];
-    // echo '<br>';
-    // echo $_POST['senha'];
+// echo '<br>';
+// echo $_POST['email'];
+// echo '<br>';
+// echo $_POST['senha'];
 
-    session_start();
+session_start();
 
-    $usuario_autenticado = false;
-    $usuario_id = null;
-    $usuario_perfil_id = null;
+$usuario_autenticado = false;
+$usuario_id = null;
+$usuario_perfil_id = null;
 
-    $perfis = array(
-      1 => 'Administrativo',
-      2 => 'Usuário'
-    );
+$perfis = array(
+  1 => 'Administrativo',
+  2 => 'Usuário'
+);
 
-    $usuarios_app = array(
-        array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
-        array('id' => 2, 'email' => 'user@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
-        array('id' => 3, 'email' => 'leo@leo.com.br', 'senha' => '1234', 'perfil_id' => 2)
-    );
+$usuarios_app = array(
+  array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
+  array('id' => 2, 'email' => 'user@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
+  array('id' => 3, 'email' => 'leo@leo.com.br', 'senha' => '1234', 'perfil_id' => 2)
+);
 
-    foreach ($usuarios_app as $user) {
-      if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
-        $usuario_autenticado = true;
-        $usuario_id = $user['id'];
-        $usuario_perfil_id = $user['perfil_id'];
-      };
-    };
+foreach ($usuarios_app as $user) {
+  if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+    $usuario_autenticado = true;
+    $usuario_id = $user['id'];
+    $usuario_perfil_id = $user['perfil_id'];
+  }
+  ;
+}
+;
 
-    if($usuario_autenticado){
-        // echo 'Usuário autenticado com sucesso!';
+if ($usuario_autenticado) {
+  // echo 'Usuário autenticado com sucesso!';
 
-        $_SESSION['autenticado'] = 'SIM';
-        $_SESSION['id'] = $usuario_id;
-        $_SESSION['perfil_id'] = $usuario_perfil_id;
-        header('Location: home.php');
-    }else{
-        // echo 'Erro de Auteticação';
-        
-        $_SESSION['autenticado'] = 'NÃO';
-        header('Location: index.php?login=erro');
-    };
+  $_SESSION['autenticado'] = 'SIM';
+  $_SESSION['id'] = $usuario_id;
+  $_SESSION['perfil_id'] = $usuario_perfil_id;
+  header('Location: home.php');
+} else {
+  // echo 'Erro de Auteticação';
+
+  $_SESSION['autenticado'] = 'NÃO';
+  header('Location: index.php?login=erro');
+}
+;
 ?>
